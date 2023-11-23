ed18d00f3012b89bec97845e776703822e16a049
AdityaChowdhury2 <adityac5102@gmail.com>
2023-11-24 02:46:23 +0600

    registration page added

---
+++ src/pages/Common/Register/Register.jsx
@@ -0,0 +1,133 @@
+import { Container } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import SocialLogin from '../../../components/Shared/SocialLogin';
+
+const Register = () => {
+	const handleSubmit = event => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+	};
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign up
+				</Typography>
+				<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete="given-name"
+								name="firstName"
+								variant="filled"
+								required
+								fullWidth
+								id="firstName"
+								label="First Name"
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								variant="filled"
+								required
+								fullWidth
+								id="lastName"
+								label="Last Name"
+								name="lastName"
+								autoComplete="family-name"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="filled"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="filled"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="new-password"
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Sign Up
+					</Button>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							Already have an account?
+							<Link
+								to={'/login'}
+								style={{
+									textDecoration: 'none',
+									color: 'darkcyan',
+									fontWeight: 600,
+								}}
+							>
+								{' '}
+								Sign in
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+			<hr />
+			<SocialLogin />
+			<Typography
+				variant="body2"
+				color="text.secondary"
+				align="center"
+				sx={{ mt: 5 }}
+			>
+				{'Copyright © '}
+				<Link to={'/'} style={{ textDecoration: 'none', color: 'darkcyan' }}>
+					<span style={{ fontWeight: 600 }}>TechTrove </span>
+				</Link>
+				{new Date().getFullYear()}
+				{'.'}
+			</Typography>
+		</Container>
+	);
+};
+
+export default Register;
